a212921cdd923518ffc86c9b809c19b8e4dcf4f0
NguyenHoangQuanke <100291264+NguyenHoangQuanke@users.noreply.github.com>
2022-10-31 15:16:44 +0700

    com

--- src/App.java
+++ src/App.java
@@ -12,7 +12,7 @@ public class App {
 					frame.setLocationRelativeTo(null);
 				} catch (Exception e) {
 					e.printStackTrace();
-//                                      Thong
+//                                      Thong123
 				}
 			}
 		});
